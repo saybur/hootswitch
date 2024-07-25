bdafbba479436b5e2bb0435003a2806701336a87
saybur <saybur@users.noreply.github.com>
2024-07-24 22:15:31 -0500

    Untested first draft of simple relay-style switch implementation.

---
+++ firmware/hardware.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 saybur
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __HARDWARE_H__
+#define __HARDWARE_H__
+
+#define LED1_PIN        0
+#define LED2_PIN        1
+#define LED3_PIN        2
+#define LED4_PIN        3
+#define LED5_PIN        4
+#define LED6_PIN        5
+#define SWITCH_PIN      6
+#define BUZZER_PIN      7
+#define A_DO_PIN        26
+#define A_DI_PIN        27
+#define C1_DO_PIN       18
+#define C1_DI_PIN       19
+#define C1_PSW_PIN      17
+#define C2_DO_PIN       15
+#define C2_DI_PIN       16
+#define C2_PSW_PIN      14
+#define C3_DO_PIN       12
+#define C3_DI_PIN       13
+#define C3_PSW_PIN      11
+#define C4_DO_PIN       9
+#define C4_DI_PIN       10
+#define C4_PSW_PIN      8
+
+#endif /* __HARDWARE_H__ */
--- firmware/main.c
+++ firmware/main.c
@@ -15,9 +15,80 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#include "hardware.h"
+#include "relay.h"
+
+static void init_hardware(void)
+{
+	// setup LEDs
+	gpio_init(LED1_PIN);
+	gpio_set_dir(LED1_PIN, GPIO_OUT);
+	gpio_init(LED2_PIN);
+	gpio_set_dir(LED2_PIN, GPIO_OUT);
+	gpio_init(LED3_PIN);
+	gpio_set_dir(LED3_PIN, GPIO_OUT);
+	gpio_init(LED4_PIN);
+	gpio_set_dir(LED4_PIN, GPIO_OUT);
+	gpio_init(LED5_PIN);
+	gpio_set_dir(LED5_PIN, GPIO_OUT);
+	gpio_init(LED6_PIN);
+	gpio_set_dir(LED6_PIN, GPIO_OUT);
+#ifdef PICO_DEFAULT_LED_PIN
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#endif
+
+	// setup input switch
+	gpio_init(SWITCH_PIN);
+	gpio_pull_up(SWITCH_PIN);
+
+	// setup buzzer
+	gpio_init(BUZZER_PIN);
+	gpio_set_dir(BUZZER_PIN, GPIO_OUT);
+
+	// setup the 'host' port
+	gpio_init(A_DO_PIN);
+	gpio_set_slew_rate(A_DO_PIN, GPIO_SLEW_RATE_SLOW);
+	gpio_set_dir(A_DO_PIN, GPIO_OUT);
+	gpio_init(A_DI_PIN);
+
+	// computer 1
+	gpio_init(C1_DO_PIN);
+	gpio_set_slew_rate(A_DO_PIN, GPIO_SLEW_RATE_SLOW);
+	gpio_set_dir(C1_DO_PIN, GPIO_OUT);
+	gpio_init(C1_DI_PIN);
+	gpio_init(C1_PSW_PIN);
+
+	// computer 2
+	gpio_init(C2_DO_PIN);
+	gpio_set_slew_rate(A_DO_PIN, GPIO_SLEW_RATE_SLOW);
+	gpio_set_dir(C2_DO_PIN, GPIO_OUT);
+	gpio_init(C2_DI_PIN);
+	gpio_init(C2_PSW_PIN);
+
+	// computer 3
+	gpio_init(C3_DO_PIN);
+	gpio_set_slew_rate(A_DO_PIN, GPIO_SLEW_RATE_SLOW);
+	gpio_set_dir(C3_DO_PIN, GPIO_OUT);
+	gpio_init(C3_DI_PIN);
+	gpio_init(C3_PSW_PIN);
+
+	// computer 4
+	gpio_init(C4_DO_PIN);
+	gpio_set_slew_rate(A_DO_PIN, GPIO_SLEW_RATE_SLOW);
+	gpio_set_dir(C4_DO_PIN, GPIO_OUT);
+	gpio_init(C4_DI_PIN);
+	gpio_init(C4_PSW_PIN);
+}
 
 int main(void)
 {
-	return 0;
+	stdio_init_all();
+	init_hardware();
+
+	relay_main();
 }
