d649f66a68e7869c577a56afe4cb346de289614a
saybur <saybur@users.noreply.github.com>
2026-04-05 14:50:11 -0500

    Switch serial to use the new mouse/keyboard drivers.

--- firmware/drivers/serial.c
+++ firmware/drivers/serial.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 saybur
+ * Copyright (C) 2024-2026 saybur
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,10 @@
 #include "debug.h"
 #include "driver.h"
 #include "hardware.h"
+#include "util.h"
 
+#include "keyboard.h"
+#include "mouse.h"
 #include "serial.h"
 
 /*
@@ -40,128 +43,10 @@
  * list.
  */
 
-#define DEFAULT_MSE_HANDLER   1
-#define DEFAULT_KBD_HANDLER   2
-#define KEYBOARD_QUEUE_DEPTH  8
-
-typedef struct {
-	uint8_t length;
-	uint8_t data[2];
-} serial_message;
-
-typedef struct {
-	uint8_t drv_idx;
-	uint8_t dhi[COMPUTER_COUNT];
-	SemaphoreHandle_t sem;
-	bool pending;
-	uint8_t accum[2];
-	uint8_t cache[2];
-} serial_mouse;
-
-typedef struct {
-	uint8_t drv_idx;
-	QueueHandle_t queue;
-} serial_keyboard;
-
-static volatile uint8_t active = 255;
 static uint8_t command;
-static serial_mouse mouse;
-static serial_keyboard keyboard;
-
-/*
- * ----------------------------------------------------------------------------
- * --- Serial Driven Mouse Driver ---------------------------------------------
- * ----------------------------------------------------------------------------
- */
-
-static void drvr_mse_reset(uint8_t comp, uint32_t ref)
-{
-	mouse.dhi[comp] = DEFAULT_MSE_HANDLER;
-	if (active == comp) {
-		if (xSemaphoreTake(mouse.sem, portMAX_DELAY)) {
-			mouse.pending = false;
-			xSemaphoreGive(mouse.sem);
-		}
-	}
-}
-
-static void drvr_mse_switch(uint8_t comp)
-{
-	active = comp;
-}
-
-static void drvr_mse_get_handle(uint8_t comp, uint32_t ref, uint8_t *hndl)
-{
-	*hndl = mouse.dhi[comp];
-}
-
-static void drvr_mse_set_handle(uint8_t comp, uint32_t ref, uint8_t hndl)
-{
-	if (hndl != 1 || hndl != 2) return;
-	mouse.dhi[comp] = hndl;
-}
-
-static void drvr_mse_talk(uint8_t comp, uint32_t ref, uint8_t reg)
-{
-	if (active != comp || reg != 0) return;
-
-	if (xSemaphoreTake(mouse.sem, portMAX_DELAY)) {
-		if (mouse.pending) {
-			if (computer_data_offer(active, mouse.drv_idx, 0, mouse.accum, 2)) {
-				mouse.pending = false;
-			}
-		}
-		xSemaphoreGive(mouse.sem);
-	}
-}
-
-static dev_driver serial_mse_driver = {
-	.default_addr = 0x03,
-	.reset_func = drvr_mse_reset,
-	.switch_func = drvr_mse_switch,
-	.talk_func = drvr_mse_talk,
-	.listen_func = NULL,
-	.flush_func = NULL,
-	.get_handle_func = drvr_mse_get_handle,
-	.set_handle_func = drvr_mse_set_handle
-};
-
-/*
- * ----------------------------------------------------------------------------
- * --- Serial Driven Keyboard Driver ------------------------------------------
- * ----------------------------------------------------------------------------
- */
-
-static void drvr_kbd_reset(uint8_t comp, uint32_t ref)
-{
-	if (active == comp) {
-		xQueueReset(keyboard.queue);
-		computer_queue_set(comp, keyboard.drv_idx, keyboard.queue);
-	}
-}
-
-static void drvr_kbd_switch(uint8_t comp)
-{
-	computer_queue_set(active, keyboard.drv_idx, NULL);
-	computer_queue_set(comp, keyboard.drv_idx, keyboard.queue);
-	active = comp;
-}
-
-static void drvr_kbd_get_handle(uint8_t comp, uint32_t ref, uint8_t *hndl)
-{
-	*hndl = DEFAULT_KBD_HANDLER;
-}
-
-static dev_driver serial_kdb_driver = {
-	.default_addr = DEFAULT_KBD_HANDLER,
-	.reset_func = drvr_kbd_reset,
-	.switch_func = drvr_kbd_switch,
-	.talk_func = NULL,
-	.listen_func = NULL,
-	.flush_func = NULL,
-	.get_handle_func = drvr_kbd_get_handle,
-	.set_handle_func = NULL
-};
+static uint8_t kbd_idx;
+static uint8_t mse_idx;
+static uint8_t mse_cache[2];
 
 /*
  * ----------------------------------------------------------------------------
@@ -171,40 +56,19 @@ static dev_driver serial_kdb_driver = {
 
 static void serial_kbd_send(bool up, uint8_t c)
 {
-	serial_message msg;
+	keyboard_message msg;
 	msg.length = 2;
 	msg.data[0] = (up ? 0x80 : 0x00) | (c & 0x7F);
 	msg.data[1] = 0xFF;
-	if (c == 0x7F) {
-		msg.data[1] = msg.data[0];
-		computer_psw(active, !up);
-	}
-	xQueueSend(keyboard.queue, &msg, 0);
+	keyboard_enqueue(kbd_idx, &msg);
 }
 
 static void serial_mse_send()
 {
-	if (xSemaphoreTake(mouse.sem, portMAX_DELAY)) {
-		if (mouse.pending) {
-			// merge existing data with current data
-			mouse.cache[0] = (mouse.cache[0] & 0x80)
-					| ((mouse.cache[0] + mouse.accum[0]) & 0x7F);
-			mouse.cache[1] = (mouse.cache[1] & 0x80)
-					| ((mouse.cache[1] + mouse.accum[1]) & 0x7F);
-		}
-		// try to send data, or if send can't be done, store
-		if (computer_data_offer(active, mouse.drv_idx, 0, mouse.cache, 2)) {
-			mouse.pending = false;
-		} else {
-			mouse.pending = true;
-			mouse.accum[0] = mouse.cache[0];
-			mouse.accum[1] = mouse.cache[1];
-		}
-		xSemaphoreGive(mouse.sem);
-	}
-
-	mouse.cache[0] &= 0x80;
-	mouse.cache[1] &= 0x80;
+	int16_t x, y;
+	uint8_t btn;
+	util_mouse_decode(mse_cache, 2, &x, &y, &btn);
+	mouse_update(mse_idx, x, y, btn);
 }
 
 void serial_enqueue(uint8_t c) {
@@ -212,11 +76,11 @@ void serial_enqueue(uint8_t c) {
 		command = c;
 		switch (command) {
 		case SER_CMD_MSE_DOWN:
-			mouse.cache[0] &= ~0x80;
+			mse_cache[0] &= ~0x80;
 			serial_mse_send();
 			break;
 		case SER_CMD_MSE_UP:
-			mouse.cache[0] |= 0x80;
+			mse_cache[0] |= 0x80;
 			serial_mse_send();
 			break;
 		case SER_CMD_MSE_APPLY:
@@ -226,10 +90,10 @@ void serial_enqueue(uint8_t c) {
 	} else {
 		switch (command) {
 		case SER_CMD_MSE_X:
-			mouse.cache[1] = (mouse.cache[1] & 0x80) | c;
+			mse_cache[1] = (mse_cache[1] & 0x80) | c;
 			break;
 		case SER_CMD_MSE_Y:
-			mouse.cache[0] = (mouse.cache[0] & 0x80) | c;
+			mse_cache[0] = (mse_cache[0] & 0x80) | c;
 			break;
 		case SER_CMD_KBD_DOWN:
 			serial_kbd_send(false, c);
@@ -246,17 +110,6 @@ void serial_enqueue(uint8_t c) {
 
 void serial_init(void)
 {
-	mouse.sem = xSemaphoreCreateMutex();
-	for (uint8_t c = 0; c < COMPUTER_COUNT; c++) {
-		mouse.dhi[c] = DEFAULT_MSE_HANDLER;
-	}
-	assert(mouse.sem != NULL);
-	mouse.cache[0] = 0x80;
-	mouse.cache[1] = 0x80;
-
-	keyboard.queue = xQueueCreate(KEYBOARD_QUEUE_DEPTH,
-				sizeof(serial_message));
-
-	driver_register(&mouse.drv_idx, &serial_mse_driver, 1);
-	driver_register(&keyboard.drv_idx, &serial_kdb_driver, 1);
+	mouse_register(&mse_idx, MOUSE_MODE_100CPI, NULL);
+	keyboard_register(&kbd_idx, NULL);
 }
