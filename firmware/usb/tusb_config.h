0780128922562aab09dd7b28db4aa414e2153f93
saybur <saybur@users.noreply.github.com>
2026-04-11 09:25:26 -0500

    Increase FIFO depth on USB to handle extra log spam.

--- firmware/usb/tusb_config.h
+++ firmware/usb/tusb_config.h
@@ -107,11 +107,11 @@
 #define CFG_TUD_VENDOR           0
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_RX_BUFSIZE   256
+#define CFG_TUD_CDC_TX_BUFSIZE   256
 
 // CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_EP_BUFSIZE   64
 
 // MSC Buffer size of Device Mass storage
 //#define CFG_TUD_MSC_EP_BUFSIZE   512
