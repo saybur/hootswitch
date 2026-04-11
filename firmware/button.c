88dfc35706fc469c259c656c6bc3f76459cbf793
saybur <saybur@users.noreply.github.com>
2026-04-11 08:44:47 -0500

    Track time on button presses.

--- firmware/button.c
+++ firmware/button.c
@@ -6,10 +6,12 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/timer.h"
 
 #include "FreeRTOS.h"
 
@@ -19,20 +21,35 @@
 
 #define SAMPLE_RATE_IN_MS 20
 
-static uint32_t sw_cnt;
+#define HOLD_TIME_SCAN   3000000L   // 3s
+#define HOLD_TIME_SWITCH 50000L     // 50ms
+
+static void button_apply(uint64_t duration)
+{
+	if (duration > HOLD_TIME_SCAN) {
+		// TODO implement
+		computer_switch(255, true);
+	} else if (duration > HOLD_TIME_SWITCH) {
+		computer_switch(255, true);
+	}
+}
 
 void button_task(void *parameters)
 {
-	const TickType_t delay_time = SAMPLE_RATE_IN_MS / portTICK_PERIOD_MS;
+	bool pressed = false;
+	uint64_t press_time = 0;
 
-	while (1) {
-		vTaskDelay(delay_time);
+	while (true) {
+		vTaskDelay(SAMPLE_RATE_IN_MS / portTICK_PERIOD_MS);
 		if (! gpio_get(SWITCH_PIN)) {
-			sw_cnt++;
+			if (! pressed) {
+				pressed = true;
+				press_time = time_us_64();
+			}
 		} else {
-			if (sw_cnt > 3) {
-				computer_switch(255, true);
-				sw_cnt = 0;
+			if (pressed) {
+				pressed = false;
+				button_apply(time_us_64() - press_time);
 			}
 		}
 	}
