ee22eb59f033c19a328f8e60a38a6d1a6ed35533
saybur <saybur@users.noreply.github.com>
2026-09-09 17:13:02 -0500

    Hack in a simple swap for the common meta/alt keys.
    This interface needs a better way to handle alternate layouts at some point.

--- flybywire/flybywire.js
+++ flybywire/flybywire.js
@@ -199,12 +199,32 @@ const transitionCodes = {
 };
 
 /*
+ * Handle swapping the Meta and Alt key for non-Mac keyboard layouts.
+ */
+const metaCheckbox = document.getElementById("swap-meta");
+function doKeyCodeSwap(code) {
+	if (metaCheckbox.checked) {
+		if (code === "MetaLeft") {
+			return "AltLeft";
+		} else if (code === "MetaRight") {
+			return "AltRight";
+		} else if (code === "AltLeft") {
+			return "MetaLeft";
+		} else if (code === "AltRight") {
+			return "MetaRight";
+		}
+	}
+	return code;
+}
+
+/*
  * Actual keycode generation is in keyboard-capture.js
  */
 function doKeyDown(code)
 {
 	if (! port) return;
 	console.log("doKeyDown", code);
+	code = doKeyCodeSwap(code);
 	const tc = transitionCodes[code];
 	if (tc != null) {
 		const a = new Uint8Array([0x02, tc]);
@@ -216,6 +236,7 @@ function doKeyUp(code)
 {
 	if (! port) return;
 	console.log("doKeyUp", code);
+	code = doKeyCodeSwap(code);
 	const tc = transitionCodes[code] + 0x80;
 	if (tc != null) {
 		const a = new Uint8Array([0x02, tc]);
